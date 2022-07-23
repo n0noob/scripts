5c741d305b5e72173ffc2145ae1d6e2621fb4bd1
n0noob <neemanoop@gmail.com>
2022-07-23 19:21:20 +0530

    time fix

--- timer/timer.py
+++ timer/timer.py
@@ -20,14 +20,14 @@ def main(stdscr):
     while True:
         stdscr.clear()
         
-        cur_time = time.localtime()
+        cur_time = time.time()
         delta_time = (time.time() - start_time)
         sec_elapsed = round(delta_time)
         min_elapsed = round(delta_time/60)
         hr_elapsed = round(delta_time/3600)
 
         stdscr.addstr(0, 1, "############## Time spent ##############")
-        stdscr.addstr(1, 1, f"{hr_elapsed:02} : {min_elapsed:02} : {sec_elapsed:02}")
+        stdscr.addstr(1, 1, f"{hr_elapsed:02} : {min_elapsed%60:02} : {sec_elapsed%60:02}")
         stdscr.addstr(2, 1, "########################################")
         stdscr.addstr(3, 1, f"{min_elapsed} minutes elapsed")
         stdscr.addstr(4, 1, f"Press [q] to stop the timer")
